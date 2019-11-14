f752d1dc4777d2d619498c661a9e76e4dec3b9d7
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-11-14 11:27:19 +0100

    Funcion "from" de RxJS

--- src/create.js
+++ src/create.js
@@ -1,8 +1,22 @@
+/**
+ * El mecanismo esencial para crear un Observable es a traves del metodo create() de la clase Observable.
+ */
 import { displayLog } from './utils';
+import { Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const hello = Observable.create(function(observer) {
+        // next() es el metodo con el cual se ejecuta il push de valores desde el Observable hacia el observer
+        observer.next("Hello");
 
+        setTimeout(() => {
+            observer.next("World");     
+        }, 2000);
+        
+    });
+
+    const subscribe = hello.subscribe(evt => displayLog(evt));
 
 
     /** end coding */
