56c1decb97a34ac6fec56b906e16882bc903d601
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-11-14 13:41:45 +0100

    Subacripciones y Observadores

--- src/create.js
+++ src/create.js
@@ -1,3 +1,11 @@
+/**
+ * Pasando un observador al netodo subscribe() de un Observable,
+ * tanto en su forma reducida como con la estructura completa.
+ * 
+ * Finalizando el flujo de datos desde el propio Observable a 
+ * traves del metodo observer.complete(), y como cancellar la 
+ * subscripcion desde fuera con el metodo unsubscribe de la subscripcion.
+ */
 import { displayLog } from './utils';
 import { Observable } from 'rxjs';
 
@@ -7,9 +15,19 @@ export default () => {
         observer.next('Hello');
         setTimeout(()=>{
             observer.next('World');
+            observer.complete();
         }, 2000);
     });
+
+    const observer = {
+        next: evt => displayLog(evt),
+        error: err => console.err('[ERR] -', err),
+        complete: () => displayLog('[DONE]')
+    }
     
-    const subscribe = hello.subscribe(evt => displayLog(evt));
+    const subscribe = hello.subscribe(observer);
+    const subscribe2  = hello.subscribe(observer);
+    subscribe.unsubscribe();
+
     /** end coding */
 }
