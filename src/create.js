55e51006c40dc7832c7614aec9767a0681da89de
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-11-14 14:35:33 +0100

    Funciones "range" y "of" de RxJS

--- src/create.js
+++ src/create.js
@@ -1,9 +1,24 @@
+/**
+ * "of" permite de emitir una sequencia variable de objetos, incluso de distinta tipologia.
+
+ */
 import { displayLog } from './utils';
+import { of, range } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const source = of(1,2,3,4,5,6);
+    const source2 = of(
+        [1,2,3],
+        'Hello World',
+        {foo:'bar'},
+        function sayHello() {
+            return 'Hello World';
+        }
+    );
+    const source3 = range(3,10);
 
-
+    const subscription = source3.subscribe(data => displayLog(data));
 
     /** end coding */
 }
