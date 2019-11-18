715f44872da7a4983450b1df6686c26cb0cbb080
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-11-18 18:26:41 +0100

    Operador "tap" de RxJS

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,15 +1,22 @@
+/**
+ * Operador "tap":
+ * - Ejecuta un efecto colateral para cada valor emitido por el Observable, pero no puede modificar en ningun modo el stream de datos del Observable. 
+ *   O sea, lo que recibe es lo que emite.
+ */
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
+        tap(val => console.log('Before map: ', val)),
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]),
+        tap(val => console.log(`After map: ${val}`)),
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
